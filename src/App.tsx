5a47c4bb87de8f81d654e18d31b166f545cf4a41
David Cedrés <david.cedresx@gmail.com>
2022-12-26 17:51:13 -0400

    chore: wip

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,34 @@ import {
   Text,
   Divider,
   CardFooter,
-  ButtonGroup,
   Button,
   Container,
   VStack,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  HStack,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 
 interface INew {
   title: string;
   description: string;
-  image: string;
   id: string;
 }
 
 const App = () => {
   const [news, setNews] = useState<INew[]>([]);
+  const [nombreQueSeEstaEscribiendo, setearNombreQueSeEscribe] =
+    useState<string>();
+  const [MODALABIERTO, SETEARMODALABIERTO] = useState<boolean>(false);
+
+  const [title, setTitle] = useState<string>();
+  const [description, setDescription] = useState<string>();
+  const [caption, setCaption] = useState<string>();
 
   useEffect(() => {
     const descargarYGuardar = async () => {
@@ -38,19 +50,51 @@ const App = () => {
     descargarYGuardar();
   }, []);
 
+  const onInput = (e: any) => {
+    setearNombreQueSeEscribe(e.target.value);
+  };
+
+  const saveNewThing = () => {
+    alert("guardandooOOOO " + nombreQueSeEstaEscribiendo);
+  };
+
+  const abrirmodal = () => {
+    SETEARMODALABIERTO(true);
+  };
+
+  const cerralModal = () => {
+    SETEARMODALABIERTO(false);
+  };
+
+  const ONSUBMNTUI = async (e: any) => {
+    e.preventDefault();
+
+    await fetch("https://63a9e4217d7edb3ae61b5c33.mockapi.io/api/Article", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title,
+        description,
+        caption,
+      }),
+    });
+
+    SETEARMODALABIERTO(false);
+  };
+
   return (
-    <Container mt={8}>
-      <VStack spacing={12}>
-        <Heading>Breaking News!</Heading>
+    <Container py={8}>
+      <VStack spacing={12} align="left">
+        <HStack display="flex" justifyContent="space-between">
+          <Heading>Breaking News!</Heading>
+          <Button onClick={abrirmodal}>Add</Button>
+        </HStack>
+
+        {news.length === 0 && <Text>Nope!</Text>}
 
         {news.map((new_) => (
-          <Card maxW="sm" key={new_.id}>
+          <Card maxW="xs" key={new_.id}>
             <CardBody>
-              <Image
-                src={new_.image}
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-              />
               <Stack mt="6" spacing="3">
                 <Heading size="md">{new_.title}</Heading>
                 <Text>{new_.description}</Text>
@@ -65,6 +109,37 @@ const App = () => {
           </Card>
         ))}
       </VStack>
+
+      <Modal isOpen={MODALABIERTO} onClose={cerralModal}>
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>Hola</ModalHeader>
+
+          <ModalBody>
+            <form onSubmit={ONSUBMNTUI}>
+              <VStack>
+                <Input
+                  name="title"
+                  placeholder="Titulo"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <Input
+                  name="description"
+                  placeholder="Description"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+                <Input
+                  name="caption"
+                  placeholder="Caption"
+                  onChange={(e) => setCaption(e.target.value)}
+                />
+                <Button type="submit">Save</Button>
+              </VStack>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 };
